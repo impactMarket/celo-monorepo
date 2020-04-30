9685e8ea3c6e28a6d8f689523e8b3e72fb11cc47
aaron deruvo <aaron@celo.org>
2020-04-30 07:47:20 -0700

    tell me a story

--- packages/mobile/storybook/index.js
+++ packages/mobile/storybook/index.js
@@ -12,8 +12,6 @@ configure(() => {
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({})
 
-// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
-// If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot)
+AppRegistry.registerComponent('Celo App', () => StorybookUIRoot)
 
 export default StorybookUIRoot
